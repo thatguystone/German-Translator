3c1baf1d3ebd79dfc82bdd9b6f2c83dd618dc276
Andrew Stone <andrew@clovar.com>
2010-11-28 23:27:22 -0500

    Tweaking styles for browsers

--- mysql.sql
+++ mysql.sql
@@ -29,7 +29,7 @@ CREATE TABLE `searches` (
   `success` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `search` (`search`,`source`,`success`)
-) ENGINE=InnoDB AUTO_INCREMENT=205468 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=205492 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,4 +95,4 @@ CREATE TABLE `verbs` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-11-28 23:20:31
+-- Dump completed on 2010-11-28 23:27:22

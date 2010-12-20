48ad4d598b5e647dbe68e9e2120706010f808c96
Andrew Stone <andrew@clovar.com>
2010-12-20 14:35:05 -0500

    Adding in GPLv3 license.

--- cims/www/index.php
+++ cims/www/index.php
@@ -1,9 +1,20 @@
 <?php
-
-ini_set('display_errors', true);
-ini_set('display_startup_errors', true);
-error_reporting(E_ALL);
-
+#
+# This file is part of Verbinator.
+# 
+# Verbinator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Verbinator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Verbinator.  If not, see <http://www.gnu.org/licenses/>.
+#
 //this is just a quick router for pages -- it does nothing special
 
 //get the page they're requesting

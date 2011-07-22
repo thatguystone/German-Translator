d9cd4494539ac6084f28d6988f1c645938fa6855
Andrew Stone <andrew@clovar.com>
2011-07-22 19:06:27 -0400

    Moving over the bookmarklet to verbinator.clovar.com

--- www/scripts/bookmarklet.js
+++ www/scripts/bookmarklet.js
@@ -4,20 +4,20 @@ javascript:(
 		css=document.createElement('link');
 		css.type='text/css';
 		css.rel='stylesheet';
-		css.href='http://deutsch/css/bookmarklet.css';
+		css.href='http://verbinator.clovar.com/css/bookmarklet.css';
 		document.getElementsByTagName('head')[0].appendChild(css);
 		
 		var addLabJs;
 		addLabJs=document.createElement('script');
 		addLabJs.type='text/javascript';
-		addLabJs.src='http://deutsch/scripts/LAB.js';
+		addLabJs.src='http://verbinator.clovar.com/scripts/LAB.js';
 		document.getElementsByTagName('head')[0].appendChild(addLabJs);
 		
 		addLabJs.onload = function() {
 			$LAB
 			.script("http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js").wait()
 			.script("https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js").wait()
-			.script("http://deutsch/scripts/bookmarklet.select.js")
+			.script("http://verbinator.clovar.com/scripts/bookmarklet.select.js")
 			.wait(function() {
 				verbinatorBookmarkletInit();
 				
@@ -25,7 +25,7 @@ javascript:(
 				if (window.frames.length > 0) {
 					for (i = 0; i < window.frames.length; i++) {
 						//fake load the bookmarklet in them
-						window.frames[i].location.href = "javascript:(function() {var verbinatorBookmarklet;verbinatorBookmarklet=document.createElement('script');verbinatorBookmarklet.type='text/javascript';verbinatorBookmarklet.src='http://deutsch/scripts/bookmarklet.js';document.getElementsByTagName('head')[0].appendChild(verbinatorBookmarklet);})();";
+						window.frames[i].location.href = "javascript:(function() {var verbinatorBookmarklet;verbinatorBookmarklet=document.createElement('script');verbinatorBookmarklet.type='text/javascript';verbinatorBookmarklet.src='http://verbinator.clovar.com/scripts/bookmarklet.js';document.getElementsByTagName('head')[0].appendChild(verbinatorBookmarklet);})();";
 					}
 				}
 			});
